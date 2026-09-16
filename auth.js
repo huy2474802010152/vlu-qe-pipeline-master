f352816f21a104c606c2cfb102493a475bd1180b
huy2474802010152 <huy.2474802010152@vanlanguni.vn>
2026-09-16 18:57:33 +0700

    Change password check in login function
    Update login validation to change password check.

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Update login validation
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "") {
         return true;
     }
     return false;
